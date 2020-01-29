7f9d7f77d905206815a9d4b1e90baaaec71163e1
rebeccapeltz <rebecca.peltz@cloudinary.com>
2020-01-29 15:53:48 -0800

    first commit

---
+++ samples/photo_album_with_jquery/app/js/photo-list/photo-list.component.ts
@@ -0,0 +1,30 @@
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {PhotoAlbum} from '../model/photo-album.service';
+
+import {Photo} from '../model/photo';
+
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'photo-list',
+  templateUrl: 'photo-list.component.html',
+  styleUrls: ['photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+
+  private photos: Observable<Photo[]>;
+
+  constructor(
+    private photoAlbum: PhotoAlbum
+  ) { }
+
+  ngOnInit(): void {
+    this.photos = this.photoAlbum.getPhotos();
+    const cld = cloudinary.Cloudinary.new({ cloud_name: "demo"});
+    cld.videoPlayer('example-player')
+
+
+  }
+}
